b9ad4dfa0f5ccad4d09ec8004abf31b604db204c
highboi <45445599+highboi@users.noreply.github.com>
2018-12-07 16:43:22 -0600

    Update highVolAutonomous.java

--- highVolAutonomous.java
+++ highVolAutonomous.java
@@ -29,37 +29,6 @@
 
 
 
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -118,11 +87,10 @@ public class highVolAutonomous extends OpMode {
      */
     @Override
     public void start() {
-    //creat variables
-    // != imput value
-      double wenchRaiseOne = 760;
-      double wenchRaiseTwo = !;
-      double inchForward = !;
+        //create variables
+        double wenchRaiseOne = 760;
+        double wenchRaiseTwo = null;
+        double inchForward = 15;
         
         //zero drive motors
         leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -135,16 +103,16 @@ public class highVolAutonomous extends OpMode {
         //low robot(raise wench)
         wenchDrive.RunMode.RUN_TO_POSITION(wenchRaiseOne);
         
-        //inch forward (imput distance in var)
+        //inch forward (input distance in var)
         leftDrive.RunMode.RUN_TO_POSITION(inchForward);
         rightDrive.RunMode.RUN_TO_POSITION(inchForward);
         
-        //final wnch raise
+        //collapse wench back into place
         wenchDrive.RunMode.RUN_TO_POSITION(wenchRaiseTwo);
         
         //servo position
         //test friday
-            double servoP = !;
+        double servoP = !;
         hook.setPosition(servoP);
        
         
